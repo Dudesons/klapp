a17ce2fe24a3d4dece575dfe31427d76f341da6d
dudesons <damdam.gold@gmail.com>
2017-11-30 22:32:42 -0800

    Just a backup idea in this file for health check

---
+++ health/consul.go
@@ -0,0 +1,16 @@
+package health
+
+//import "fmt"
+//
+//func ConsulHealth() {
+//	client, err := c.BaseCommand.HTTPClient()
+//	if err != nil {
+//		c.UI.Error(fmt.Sprintf("Error connecting to Consul agent: %s", err))
+//		return 1
+//	}
+//	_, err = client.Agent().NodeName()
+//	if err != nil {
+//		c.UI.Error(fmt.Sprintf("Error querying Consul agent: %s", err))
+//		return 1
+//	}
+//}
